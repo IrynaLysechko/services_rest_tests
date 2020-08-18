905ab3f883a3993a3d787a97a196286be465f556
Iryna <iralysechko1497@gmail.com>
2020-08-18 17:00:08 +0300

    rest tests

--- src/test/java/com/epam/BaseTest.java
+++ src/test/java/com/epam/BaseTest.java
@@ -4,16 +4,8 @@ import com.epam.client.RestAuthor;
 import com.epam.client.RestBook;
 import com.epam.client.RestGenre;
 import com.epam.entity.Author;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.Response;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.epam.constants.CodeConstants.*;
 
 public class BaseTest {
     protected static Author author = new Author();;
